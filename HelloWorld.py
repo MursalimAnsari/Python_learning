047a2b29243607bd7720001867b6d8d959473644
MursalimAnsari <mursalim.ansari.1998@gmail.com>
2023-09-10 13:59:22 +0530

    explored more about python basics

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,11 +3,17 @@ print("Hello World !")
 
 
 first_name = "mursalim"
-print(first_name)
-
 last_name = "ansari"
 
+print(first_name)
 print(last_name)
+
+print(type( "type of first name is: " +first_name))
+print(type( "type of first name is: " +last_name))
+
+
+
+
 
 print("concatenate two strings")
 
@@ -15,4 +21,19 @@ full_name = first_name+" " +last_name
 print("full name is: " +full_name)
 
 print('testing single quote comment')
+
+
+
+# working with integer value
+
+a= int(20)
+b= int(60)
+
+sum = a+b
+
+print(sum)
+
+print(type(a))
+print(type(b))
+
 
