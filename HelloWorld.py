02ddb6d0cc61a46e864308262a5aad2001f7b1c7
MursalimAnsari <mursalim.ansari.1998@gmail.com>
2023-09-10 13:00:20 +0530

    adding hello world file in python

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+print("Hello World !")
+
+
+
+first_name = "mursalim"
+print(first_name)
+
+last_name = "ansari"
+
+print(last_name)
+
+print("concatenate two strings")
+
+full_name = first_name+" " +last_name
+print("full name is: " +full_name)
+
