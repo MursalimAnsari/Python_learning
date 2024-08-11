04681aa14bda07977bf8788a154a3a24d6ef0b70
MursalimAnsari <mursalim.ansari.1998@gmail.com>
2024-08-12 01:01:08 +0530

    csv read and write operations

---
+++ data_types/dictionary_test_01.py
@@ -0,0 +1,12 @@
+users  = {1 :"Mursalim" , 2:"Sameer", 3:"Anu"}
+
+print(type(users))
+
+print(users)
+# getting elements by key
+print(users[1])
+
+#get using get()
+print(users.get(2))
+
+users.items()
