e7fc1dc880fd662d006a61971c960e40047b6646
MursalimAnsari <mursalim.ansari.1998@gmail.com>
2024-08-12 01:01:08 +0530

    csv read and write operations

--- data_types/set/set_test_01.py
+++ data_types/set/set_test_01.py
@@ -1,9 +1,9 @@
 
 numbers  = [1,2,3,2,2,4,3,3,5,6,6,7]
-print("list:", numbers)
+# print("list:", numbers)
 
 unique_numbers = set(numbers)
-print("set:", unique_numbers)
+# print("set:", unique_numbers)
 
 # unique_numbers.add(10)
 # print("set after adding element:", unique_numbers)
@@ -14,3 +14,19 @@ print("set:", unique_numbers)
 # unique_numbers.pop()
 # print(unique_numbers)
 
+
+names_one = set({'mursalim', 'anu', 'sameer','mursalim'})
+names_two = set({'sameer', 'aavesh', 'azibur'})
+
+print(names_two.intersection(names_one))
+
+
+# print(hash(name))
+
+
+# name = {'s','a', 'm', 'e', 'e', 'r'}
+# print(name)
+
+cities = frozenset({'hyderabad', 'delhi', 'bengaluru', 'pune'})
+# for city in cities:
+#     print(city, end=',')
